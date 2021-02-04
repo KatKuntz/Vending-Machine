6dda2d882592d7fde94092b2dab24b5eb3bcfb59
Kat Kuntz <rezonantcheetah@gmail.com>
2021-02-04 14:45:40 -0500

    Add StubProductProvider to test project

---
+++ Capstone/dotnet/CapstoneTests/Providers/StubProductProvider.cs
@@ -0,0 +1,21 @@
+﻿using Capstone.Products;
+using Capstone.Providers;
+using System.Collections.Generic;
+
+namespace CapstoneTests.Providers
+{
+    class StubProductProvider : IProductProvider
+    {
+        List<Product> products = new List<Product>();
+
+        public IList<Product> GetProducts()
+        {
+            return products;
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
