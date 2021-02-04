9451dabc8034aacf24eb555f5f680cd3b8719799
Kat Kuntz <rezonantcheetah@gmail.com>
2021-02-04 13:47:37 -0500

    Store number sold instead of revenue.
    I reread the readme and it looks like we need to output number sold for
    the report, not the total money it brought in.

--- Capstone/dotnet/Capstone/Products/Product.cs
+++ Capstone/dotnet/Capstone/Products/Product.cs
@@ -3,7 +3,7 @@
     public abstract class Product
     {
         public int CurrentQuantity { get; private set; } = 5;
-        public int CurrentRevenue { get; private set; } = 0;
+        public int NumberSold { get; private set; } = 0;
         public string ProductName { get; }
         public decimal Price { get; }
         public abstract string GetMessage();
