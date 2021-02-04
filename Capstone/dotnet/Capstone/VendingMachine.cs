92dc05aa03884324845ba7318924a1af4900d76f
nathan groehl <ndgroehl@gmail.com>
2021-02-04 18:26:43 -0500

    added Log functionality to methods, added GiveChange and removed SalesReport

--- Capstone/dotnet/Capstone/VendingMachine.cs
+++ Capstone/dotnet/Capstone/VendingMachine.cs
@@ -1,54 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Capstone.Products;
 using Capstone.Providers;
-using Capstone.Products;
 using Capstone.Util;
+using System.Collections.Generic;
 
 namespace Capstone
 {
-    class VendingMachine
+    public class VendingMachine
     {
-        decimal CurrentBalance { get; set; } = 0;
+        public decimal CurrentBalance { get; private set; } = 0;
+        public IDictionary<string, Product> CurrentInventory { get; }
         public VendingMachine(IProductProvider provider)
-            {
+        {
+            CurrentInventory = provider.GetProducts();
+        }
 
-            }
-        Dictionary<string, Product> VendMachDict = new Dictionary<string, Product>();
         public void FeedMoney(decimal deposit)
         {
             CurrentBalance += deposit;
-            Console.WriteLine($"Balance of ${CurrentBalance}");
-            Logger.Log($"FEED MONEY: ${deposit} \n{CurrentBalance}");
-            //verify \n means new line
+            Logger.Log($"FEED MONEY: ${deposit} {CurrentBalance}");
         }
-        public void GetInventory()
+        /*public void GetInventory()
         {
-            foreach (KeyValuePair<string, Product> indivProduct in VendMachDict)
+            foreach (KeyValuePair<string, Product> indivProduct in CurrentInventory)
             {
                 if(indivProduct.Value.CurrentQuantity>0)
                 {
                     Console.WriteLine($"{indivProduct.Key}. {indivProduct.Value.ProductName} for ${indivProduct.Value.Price}");
                 }
             }
+        }*/
+        public decimal GetPrice(string slotId)
+        {
+            return CurrentInventory[slotId].Price;
         }
         public void Dispense(string slotId)
         {
-            if (CurrentBalance>=VendMachDict[slotId].Price)
-            {
-                CurrentBalance =- VendMachDict[slotId].Price;
-                VendMachDict[slotId].SellProduct();
-                Console.WriteLine($"{VendMachDict[slotId].ProductName} purchased for {VendMachDict[slotId].Price} with balance remaining of {CurrentBalance}");
-                Console.WriteLine(VendMachDict[slotId].GetMessage());
-            }
-            else
-            {
-                Console.WriteLine($"Current balance is less than the item price. You need ${VendMachDict[slotId].Price-CurrentBalance} to make this purchase");
-            }
+            CurrentBalance = -GetPrice(slotId);
+            CurrentInventory[slotId].SellProduct();
+            Logger.Log($"{CurrentInventory[slotId].ProductName} {slotId} ${CurrentBalance+GetPrice(slotId)} ${CurrentBalance}");
+            //Console.WriteLine($"{CurrentInventory[slotId].ProductName} purchased for {CurrentInventory[slotId].Price} with balance remaining of {CurrentBalance}");
+            //Console.WriteLine(CurrentInventory[slotId].GetMessage());
+            //Console.WriteLine($"Current balance is less than the item price. You need ${CurrentInventory[slotId].Price-CurrentBalance} to make this purchase");
         }
-        public void GetSalesReport()
+        public void ReturnChange()
         {
-
+            decimal initialBalance = CurrentBalance;
+            int numOfQuarters = 0;
+            int numOfDimes = 0;
+            int numOfNickles = 0;
+            int numOfPennies = 0;
+            while (CurrentBalance >= 0.25M)
+            {
+                CurrentBalance = -.25M;
+                numOfQuarters++;
+            }
+            while (CurrentBalance >= 0.10M)
+            {
+                CurrentBalance = -.10M;
+                numOfDimes++;
+            }
+            while (CurrentBalance >= 0.05M)
+            {
+                CurrentBalance = -.05M;
+                numOfNickles++;
+            }
+            while (CurrentBalance >= 0.01M)
+            {
+                CurrentBalance = -.01M;
+                numOfPennies++;
+            }
+            Logger.Log($"GIVE CHANGE: ${initialBalance} ${CurrentBalance}");
         }
     }
 }
