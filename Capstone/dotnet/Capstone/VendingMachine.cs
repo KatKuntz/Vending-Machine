0e4a75a1f55bbeb88038502588fbdd332cfdb580
nathan groehl <ndgroehl@gmail.com>
2021-02-04 14:54:01 -0500

    Initial VendingMachine

---
+++ Capstone/dotnet/Capstone/VendingMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    class VendingMachine
+    {
+        decimal CurrentBalance { get; set; } = 0;
+        Dictionary<string, Products.Product> VendMachDict = new Dictionary<string, Products.Product>();
+        public VendingMachine(string slotID)
+        {
+        }
+        public void FeedMoney(int deposit)
+        {
+            CurrentBalance += deposit;
+        }
+        public void GetInventory()
+        {
+
+        }
+        public void Dispense(string slotId)
+        {
+            if (CurrentBalance>=VendMachDict[slotId].Price)
+            {
+                CurrentBalance =- VendMachDict[slotId].Price;
+                Console.WriteLine($"{VendMachDict[slotId].ProductName} purchased for {VendMachDict[slotId].Price} with balance remaining of {CurrentBalance}");
+                Console.WriteLine(VendMachDict[slotId]);
+            }
+            else
+            {
+
+            }
+        }
+        public void GetSalesReport()
+        {
+
+        }
+    }
+}
