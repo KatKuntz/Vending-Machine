4b92c09be191f99a74fcb83f23e964d2647e5c55
Kat Kuntz <rezonantcheetah@gmail.com>
2021-02-04 11:49:29 -0500

    Add basic logger

---
+++ Capstone/dotnet/Capstone/Util/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Capstone.Util
+{
+    static class Logger
+    {
+        private const string logFile = @"Log.txt";
+
+        public static void Log(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(logFile))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
