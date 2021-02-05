1c93f1217b4b5636d4f9f6f2cd8c9be2cab579e1
nathan groehl <ndgroehl@gmail.com>
2021-02-05 09:10:01 -0500

    Adding paremeter for GetSalesReport

--- Capstone/dotnet/Capstone/Util/SalesReport.cs
+++ Capstone/dotnet/Capstone/Util/SalesReport.cs
@@ -8,7 +8,7 @@ namespace Capstone.Util
 {
     public class SalesReport
     {
-        public void GetSalesReport()
+        public static void GetSalesReport(VendingMachine vendingMachine)
         {
             try
             {
@@ -18,10 +18,7 @@ namespace Capstone.Util
                 using (StreamWriter sw = new StreamWriter(outputFullPath))
                 {
                     decimal totalSales = 0;
-                    IDictionary<string, Product> currentInventory= VendingMachine.GetCurrentInventory();
-                    //need to create GetCurrentInventory in VendingMachine
-                    foreach (KeyValuePair<string, Product> salesReportEntry in currentInventory)
-                       
+                    foreach (KeyValuePair<string, Product> salesReportEntry in vendingMachine.CurrentInventory)
                     {
                         sw.WriteLine($"{salesReportEntry.Value.ProductName}|{salesReportEntry.Value.NumberSold}");
                         totalSales += salesReportEntry.Value.NumberSold;
@@ -33,7 +30,6 @@ namespace Capstone.Util
             {
                 throw new Exception("Could not create or write file for Sales Report", e);
             }
-
         }
     }
 }
