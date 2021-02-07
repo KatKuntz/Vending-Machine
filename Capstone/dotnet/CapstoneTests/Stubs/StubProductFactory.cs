5d77061f507deaa27dee5ca6b9a45dd051f455c0
Kat Kuntz <rezonantcheetah@gmail.com>
2021-02-07 15:14:34 -0500

    Add StubProductFactory

---
+++ Capstone/dotnet/CapstoneTests/Stubs/StubProductFactory.cs
@@ -0,0 +1,35 @@
+﻿using Capstone.Products;
+using System;
+
+namespace CapstoneTests.Stubs
+{
+    class StubProductFactory : IProductFactory
+    {
+        private readonly bool throwException;
+
+        public string ProductName { get; private set; }
+        public decimal Price { get; private set; }
+        public string ProductType { get; private set; }
+        public int InitialQuantity { get; private set; }
+
+        public StubProductFactory(bool throwException)
+        {
+            this.throwException = throwException;
+        }
+
+        public Product MakeProduct(string productName, decimal price, string productType, int initialQuantity)
+        {
+            if (throwException)
+            {
+                throw new ArgumentException();
+            }
+
+            ProductName = productName;
+            Price = price;
+            ProductType = productType;
+            InitialQuantity = initialQuantity;
+
+            return null;
+        }
+    }
+}
